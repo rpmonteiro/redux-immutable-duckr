001c2bc7e72ea26365230c16fba76be78a2dc3d7
rpmonteiro <ric.p.monteiro@gmail.com>
2016-05-21 18:24:00 +0100

    setup the navbar

---
+++ app/components/Navigation/Navigation.js
@@ -0,0 +1,34 @@
+import React, { PropTypes } from 'react'
+import { Link } from 'react-router'
+
+
+function NavLinks ({isAuthed}) {
+  return isAuthed === true
+  ? <ul>
+      <li><Link to='/'>{'Home'}</Link></li>
+    </ul>
+  : null
+}
+
+function ActionLinks ({isAuthed}) {
+  return isAuthed === true
+  ? <ul>
+      <li>{'NEW DUCK'}</li>
+      <li><Link to='/logout'>{'Logout'}</Link></li>
+    </ul>
+  : <ul>
+      <li><Link to='/'>{'Home'}</Link></li>
+      <li><Link to='/auth'>{'Login'}</Link></li>
+    </ul>
+}
+
+export default function Navigation ({isAuthed}) {
+  return (
+    <div>
+      <nav>
+        <NavLinks isAuthed={isAuthed} />
+        <ActionLinks isAuthed={isAuthed} />
+      </nav>
+    </div>
+  )
+}
--- app/containers/Main/MainContainer.js
+++ app/containers/Main/MainContainer.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { container, innerContainer } from './styles.css'
+import { Navigation } from 'components'
 
 const MainContainer = React.createClass({
   render () {
     return (
       <div className={container}>
+        <Navigation isAuthed={false}/>
         <div className={innerContainer}>
           {this.props.children}
         </div>
