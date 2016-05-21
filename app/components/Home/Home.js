9324e48b276d782a5d8538aeb77b57b3a5df18d6
rpmonteiro <ric.p.monteiro@gmail.com>
2016-05-21 18:00:26 +0100

    created the home component and container

---
+++ app/components/Home/Home.js
@@ -0,0 +1,10 @@
+import React, { PropTypes } from 'react'
+
+export default function Home (props) {
+  return (
+    <div>
+      <p>{'Duckr'}</p>
+      <p>{'The real time, cloud based, modular, Duckr'}</p>
+    </div>
+  )
+}
