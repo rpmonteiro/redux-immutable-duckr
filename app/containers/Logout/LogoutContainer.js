6a6558e8fa0eb47e1c0068f9f2fc5a03a5f05213
rpmonteiro <ric.p.monteiro@gmail.com>
2016-05-22 15:26:00 +0100

    added the logout container/component

---
+++ app/containers/Logout/LogoutContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Logout } from 'components'
+
+const LogoutContainer = React.createClass({
+  render () {
+    return (
+      <Logout />
+    )
+  },
+})
+export default LogoutContainer
