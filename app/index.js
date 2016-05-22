985234ace20180553b49f6f6f267b482c3ce1b6d
rpmonteiro <ric.p.monteiro@gmail.com>
2016-05-22 11:24:09 +0100

    created the store by passing a reducer function to it, which will populate the store. Sent the store down using the provider from react-redux.

--- app/index.js
+++ app/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import routes from './config/routes'
-import { Provider } from 'redux'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import users from 'redux/modules/users'
+
+const store = createStore(users)
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     {routes}
   </Provider>,
   document.getElementById('app')
